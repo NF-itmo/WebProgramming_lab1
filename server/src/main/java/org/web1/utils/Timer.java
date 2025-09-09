aeb8dc19c4ccc18aa74cced9766b688db3e7b56e
NF-coder <resetnikovs72@gmail.com>
2025-09-09 03:22:07 +0300

    java-fcgi-timer: nanosecond timer added
    Now u can see how much time request handling took

---
+++ server/src/main/java/org/web1/utils/Timer.java
@@ -0,0 +1,11 @@
+package org.web1.utils;
+
+public class Timer {
+    long startTime;
+    public void start() {
+        this.startTime = System.nanoTime();
+    }
+    public long stop() {
+        return System.nanoTime() - this.startTime;
+    }
+}
